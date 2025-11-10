03bc08f6b85e691fa68feae62e2939352e18887c
OliverMaxe <BueningCucinotta345@gmail.com>
2025-11-10 16:05:22 +0800

    frontend base

---
+++ frontend/next.config.ts
@@ -0,0 +1,40 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+  headers() {
+    // Required by FHEVM 
+    return Promise.resolve([
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+      {
+        source: '/:path*.wasm',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/wasm',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ]);
+  }
+};
+
+export default nextConfig;
